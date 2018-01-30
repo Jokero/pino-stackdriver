3dc80deda8fd56c1321215a4f5c4de84b87f62d5
Dmitry Kirilyuk <Dmitry.Kirilyuk@infobip.com>
2018-01-30 13:30:30 +0300

    Extract format to separate module

--- bin.js
+++ bin.js
@@ -2,41 +2,7 @@
 
 const split = require('split2');
 const Parse = require('fast-json-parse');
-
-const PINO_TO_STACKDRIVER = {
-  10: 'DEBUG',
-  20: 'DEBUG',
-  30: 'INFO',
-  40: 'WARNING',
-  50: 'ERROR',
-  60: 'CRITICAL',
-};
-
-const format = ({
-  time,
-  pid,
-  hostname,
-  level,
-  msg,
-  stack,
-  v,
-  type,
-  ...rest
-}) => {
-  let message = stack || msg;
-  const output = {
-    ...rest,
-    severity: PINO_TO_STACKDRIVER[level] || 'UNKNOWN',
-    timestamp: time,
-    message,
-  };
-
-  if (stack && msg) {
-    output.msg = msg;
-  }
-
-  return output;
-};
+const format = require('./lib/format');
 
 const isPinoLine = line => {
   return (
---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const format = require('./lib/format');
+
+exports.format = format;
